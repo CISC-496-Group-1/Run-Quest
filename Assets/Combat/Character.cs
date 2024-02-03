92c683d98011789042999a02a203586b59bd41cd
Nerax225 <lyc141617@gmail.com>
2024-02-02 19:40:32 -0500

    redesign the combat system

--- Assets/Combat/Character.cs
+++ Assets/Combat/Character.cs
@@ -1,35 +1,10 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    public int HP;
-    public int Speed;
-    public int Damage;
-    // Constructor
-    public Character(int hp, int speed, int damage)
-    {
-        HP = hp;
-        Speed = speed;
-        Damage = damage;
-    }
-
-    // Attack method
-    public void Attack(Character target)
-    {
-        target.HP -= this.Damage;
-        // Trigger attack animation here (later steps will cover this)
-    }
-
-    // Check if defeated
-    public bool IsDefeated()
-    {
-        return HP <= 0;
-    }
-
-    // Heal method
-    public void Heal(Character target, int healPoint)
-    {
-        target.HP += healPoint;
-    }
+    public string name;
+    public int health = 100;
+    public int attackDamage = 20; // This is a simplified attack system.
 }
