57ffb3b13c80a3115223256b97ee820b4bf420b1
Pratyush <pratyush.kaintura@hotmail.com>
2024-03-02 16:23:20 -0500

    Create save file for new user

--- Assets/Scripts/User Interface/PlayerStats.cs
+++ Assets/Scripts/User Interface/PlayerStats.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
+using CI.QuickSave;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -13,12 +14,26 @@ public class PlayerStats : MonoBehaviour
 
     public Text playerText;
 
+    private QuickSaveReader reader;
+    private QuickSaveWriter writer;
+
     void Start()
     {
-        strength = 1;
-        defense = 1;
-        magicDamage = 1; 
-        speed = 1;
+        if (QuickSaveReader.RootExists("Stats"))
+        {
+            writer = QuickSaveWriter.Create("Stats");
+            writer.Write("Strength", 1);
+            writer.Write("Defense", 1);
+            writer.Write("Magic Damage", 1);
+            writer.Write("Speed", 1);
+        }
+        writer = QuickSaveWriter.Create("Stats");
+        reader = QuickSaveReader.Create("Stats");
+
+        strength = reader.Read<int>("Strength");
+        defense = reader.Read<int>("Defense");
+        magicDamage = reader.Read<int>("Magic Damage");
+        speed = reader.Read<int>("Speed");
     }
 
     public void addStrength(int amountToAdd)
