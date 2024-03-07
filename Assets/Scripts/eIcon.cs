be8eb514c750aa62896974e7700287ce1320ec39
ZhouJun Pan <panfrank666@gmail.com>
2024-03-07 13:17:21 -0500

    Since it's just a small update, I just diretly added it to merged branch. I add the interaction E button when the player gets close to the chest. The script is called eicon and it is only attached to the wooden chest.

---
+++ Assets/Scripts/eIcon.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayEIcon : MonoBehaviour
+{
+    public GameObject eIcon; // 指向你的"E"图标的引用
+
+    // 当玩家角色进入2D触发器区域时调用
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // 确保只有玩家可以触发事件
+        {
+            eIcon.SetActive(true); // 显示"E"图标
+        }
+    }
+
+    // 当玩家角色离开2D触发器区域时调用
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // 确保只有玩家可以触发事件
+        {
+            eIcon.SetActive(false); // 隐藏"E"图标
+        }
+    }
+}
+
